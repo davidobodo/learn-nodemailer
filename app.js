2ea4df681923a87b13c3b696bdb2465eb0277f79
David Obodo <obododavid5@gmail.com>
2020-04-19 03:49:43 +0100

    add statis folder

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const nodemailer = require('nodemailer');
+const path = require('path')
 
 //initialize app variable
 const app = express();
@@ -10,6 +11,10 @@ const app = express();
 //view engine setup
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+
+//static folder
+app.use('/public', express.static(path.join(__dirname, 'public')))
 
 
 //body parser middleware
