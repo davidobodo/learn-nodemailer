810ab39c6fada13231c7258f8d3e4fb2819f2cee
David Obodo <obododavid5@gmail.com>
2020-04-19 03:46:43 +0100

    set up middlewares -handlebars for view engine -body parser middleware from its github page

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ const nodemailer = require('nodemailer');
 //initialize app variable
 const app = express();
 
+
+//view engine setup
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
 //create a route - this is just for index page
 app.get('/', (req, res) => {
+    //wht this route sends
     res.send("Hello")
 })
 
