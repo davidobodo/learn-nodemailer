b2c3aebb50e646a2e691875f87d96de517b3fdb4
David Obodo <obododavid5@gmail.com>
2020-04-19 05:09:10 +0100

    sending emails with summy account

--- app.js
+++ app.js
@@ -28,8 +28,49 @@ app.get('/', (req, res) => {
     res.render('contact')
 })
 
-app.post('/send', (req, res) => {
-    console.log(req.body)
+app.post('/send', async (req, res) => {
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>  
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+    `;
+
+    let testAccount = await nodemailer.createTestAccount();
+
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: testAccount.user, // generated ethereal user
+            pass: testAccount.pass // generated ethereal password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+        from: '"Nodemailer Contact 👻" <davidobodo@rocketmail.com>', // sender address
+        to: "obododavid5@gmail.com", // list of receivers
+        subject: "Node contact request", // Subject line
+        text: "Hello world?", // plain text body
+        html: output// html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render('contact', { msg: 'Email has been sent' })
 })
 
 app.listen(5000, () => console.log('Server started...'))
